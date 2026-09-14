5c5803d81f3dc4e1ffd4111034b4f1b5799bae2f
henrique <henriquesilvacarvalho576@gmail.com>
2026-09-14 15:56:26 -0300

    Henrique

---
+++ estudantes/src/main/java/br/com/senac/df/estudantes/Estudantes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.senac.df.estudantes;
+
+/**
+ *
+ * @author henrique62977716
+ */
+public class Estudantes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
